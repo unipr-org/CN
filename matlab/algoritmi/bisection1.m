fdfca7ebde74dd444bbb731982dd3cbeb3e6d382
MarkGotLasagna <marcorondelli1@gmail.com>
2024-06-11 14:06:06 +0200

    Alternative bisection using recursion

--- matlab/algoritmi/bisection1.m
+++ matlab/algoritmi/bisection1.m
@@ -1,50 +1,35 @@
-%% BISECTION1 Find roots of a function in a range using max iterations
-% bisection1(f,a,b,tol,max_iter) works similarly to bisection but takes as
-% additional argument max_iter indicating the maximum number of iterations
-% allowed to find a root.
+%% BISECTION1 Find roots of a function in a range
+% bisection(f,a,b,tol) finds the root of f given as function
+% handle in the range a to b with tol referring to the
+% tolerance accepted. This method uses recursion.
 %
-% Examples:
+% Examples
 %   f = @(x) sin(x);
-%   [v, y, iter] = bisection1(f, 3, 4, 10^-10, 100);
-%   
-% See also BISECTION
-function [v, y, iter] = bisection1(f, a, b, tol, max_iter)
+%   [x] = bisection(f, 3, 4, 10^-10);
+%
+% See also BISECTION, SECANT, NEWTON
+function [x] = bisection1(f, a, b, tol)
 
-    if sign(f(a)) == sign(f(b))
+    % First midpoint
+    x = (a + b) * 0.5; % (a+b)/2
+    
+    % If first midpoint is root
+    if f(x) == 0
+        return
+    end
+
+    % Check zero existence in specified interval
+    if sign(f(a)) == sign(f(b)) % f(a) * f(b) > 0
         error('Interval does not satisfy the conditions.')
     end
 
-    % Number of iterations
-    iter = 0;
-
-    % Preallocate for speed
-    v = zeros(1, iter);
-    y = zeros(1, iter);
-
-    % Check error and number of iterations
-    while (abs(b-a) > tol) && (iter < max_iter)
-        iter = iter + 1;
-
-        % First midpoint
-        x = (a + b) * 0.5;
-
-        % Store in column vector
-        v(iter) = x;            % Midpoints
-        y(iter) = abs(f(x));    % Residual
-
-        if sign(f(a)) == sign(f(x)) % Right side f(a)*f(x)>0
-            a = x;
-        elseif f(x) == 0 || f(a) == 0
-            disp('Found root')
-            disp(x)
-            return
-        else % Left side
-            b = x;
-        end
+    % Resiual criteria
+    if abs(b - a) < tol
+        return
+    elseif sign(f(a)) == sign(f(x)) % Right side
+        x = bisection(f, x, b, tol);
+    elseif sign(f(b)) == sign(f(x)) % Left side
+        x = bisection(f, a, x, tol);
     end
-
-    % Truncate unused elements
-    v = v(1:iter)';
-    y = y(1:iter)';
 
 end
